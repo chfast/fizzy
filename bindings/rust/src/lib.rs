2224dadc4851d8cf1c725e8a3ea9d612e68d0200
Alex Beregszaszi <alex@rtfs.hu>
2020-10-02 23:22:19 +0100

    Use result

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -25,12 +25,12 @@ impl Drop for Module {
     }
 }
 
-pub fn parse<T: AsRef<[u8]>>(input: &T) -> Option<Module> {
+pub fn parse<T: AsRef<[u8]>>(input: &T) -> Result<Module, ()> {
     let ptr = unsafe { sys::fizzy_parse(input.as_ref().as_ptr(), input.as_ref().len()) };
     if ptr.is_null() {
-        return None;
+        return Err(());
     }
-    Some(Module {
+    Ok(Module {
         ptr: unsafe { NonNull::new_unchecked(ptr) },
     })
 }
@@ -47,14 +47,14 @@ impl Drop for Instance {
 
 impl Module {
     // TODO: support imported functions{
-    pub fn instantiate(self) -> Option<Instance> {
+    pub fn instantiate(self) -> Result<Instance, ()> {
         let ptr = unsafe { sys::fizzy_instantiate(self.ptr.as_ptr(), std::ptr::null_mut(), 0) };
         // Forget Module (and avoid calling drop) because it has been consumed by instantiate (even if it failed).
         core::mem::forget(self);
         if ptr.is_null() {
-            return None;
+            return Err(());
         }
-        Some(Instance {
+        Ok(Instance {
             ptr: unsafe { NonNull::new_unchecked(ptr) },
         })
     }
@@ -153,25 +153,25 @@ mod tests {
 
     #[test]
     fn parse_wasm() {
-        assert!(parse(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00]).is_some());
-        assert!(parse(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x01]).is_none());
+        assert!(parse(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00]).is_ok());
+        assert!(parse(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x01]).is_err());
     }
 
     #[test]
     fn instantiate_wasm() {
         let module = parse(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00]);
-        assert!(module.is_some());
+        assert!(module.is_ok());
         let instance = module.unwrap().instantiate();
-        assert!(instance.is_some());
+        assert!(instance.is_ok());
     }
 
     #[test]
     fn execute_wasm() {
         let input = hex::decode("0061736d01000000010e036000006000017f60027f7f017f030504000102000a150402000b0400412a0b0700200020016e0b0300000b").unwrap();
         let module = parse(&input);
-        assert!(module.is_some());
+        assert!(module.is_ok());
         let instance = module.unwrap().instantiate();
-        assert!(instance.is_some());
+        assert!(instance.is_ok());
         let mut instance = instance.unwrap();
 
         let result = instance.execute(0, &[]);
