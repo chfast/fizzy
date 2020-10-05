77c28cb4944e10d5a3654abfffe95d5e15aa2536
Alex Beregszaszi <alex@rtfs.hu>
2020-10-05 15:45:33 +0100

    Document execute.hpp

--- lib/fizzy/execute.hpp
+++ lib/fizzy/execute.hpp
@@ -18,11 +18,14 @@
 
 namespace fizzy
 {
-// The result of an execution.
+/// The result of an execution.
 struct ExecutionResult
 {
+    /// This is true if the execution has trapped. No other fields are valid.
     const bool trapped = false;
+    /// This is true if the `value()` returns valid data.
     const bool has_value = false;
+    /// This single result of execution.
     const Value value{};
 
     /// Constructs result with a value.
@@ -36,9 +39,23 @@ struct ExecutionResult
 constexpr ExecutionResult Void{true};
 constexpr ExecutionResult Trap{false};
 
-// Execute a function on an instance.
+/// Execute a function on an instance.
+///
+/// @param instance The instance, see `instance.hpp`.
+/// @param func_idx The function index. MUST be a valid index, otherwise undefined behaviour
+/// (including crash) happens.
+/// @param args     The pointer to the arguments. The number of items must match the expected number
+/// of input parameters of the function, otherwide underfiend behaviour (including crash) happens.
+/// @param depth    The call depth (indexing starts at 0). Can be left at the default setting.
 ExecutionResult execute(Instance& instance, FuncIdx func_idx, const Value* args, int depth = 0);
 
+/// Execute a function on an instance.
+///
+/// @param instance The instance, see `instance.hpp`.
+/// @param func_idx The function index. MUST be a valid index, otherwise undefined behaviour
+/// (including crash) happens.
+/// @param args     The arguments. The number of items must match the expected number
+/// of input parameters of the function, otherwide underfiend behaviour (including crash) happens.
 inline ExecutionResult execute(
     Instance& instance, FuncIdx func_idx, std::initializer_list<Value> args)
 {
